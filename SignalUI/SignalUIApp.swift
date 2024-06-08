406496e12e5be374b3c1ef5e2d877d8a4674ea02
Vladislav Rudenko <vladisrud@gmail.com>
2024-06-08 13:29:24 +0400

    Initial Commit

---
+++ SignalUI/SignalUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalUIApp.swift
+//  SignalUI
+//
+//  Created by Влад Руденко on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SignalUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
